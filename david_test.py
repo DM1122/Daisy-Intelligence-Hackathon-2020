fb5c29e3f77e41717c8d952fbd284502d9496089
DM1122 <34867698+DM1122@users.noreply.github.com>
2020-01-25 13:58:23 -0500

    Complete searchImage() for labels

--- david_test.py
+++ david_test.py
@@ -3,57 +3,71 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-debug = True
+debug = False
 
 def searchImage(img_path, template_path):
     '''
     Returns True or False if template is found in image.
     '''
+    threshold = 0.9
+
 
     img = cv2.imread(img_path, 0)           # load in grayscale
     template = cv2.imread(template_path,0)
 
-    res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+    
+    loc = np.where(res >= threshold)
 
-    bound_tl = max_loc
-    bound_br = (max_loc[0]+template.shape[1], max_loc[1] + template.shape[0])
+    
+    if loc[0].shape[0] != 0:
+        flag = True
+    else:
+        flag = False
 
-    cv2.rectangle(img, bound_tl, bound_br, 255, 2)
+        
+
 
     if debug:
-        plt.subplot(1,2,1)
-        plt.imshow(res, cmap = 'gray')
-        plt.title('Matching Result')
-        plt.subplot(1,2,2),plt.imshow(img,cmap = 'gray')
-        plt.title('Detected Point')
 
-    plt.show()
+        for point in zip(*loc[::-1]):
+            cv2.imwrite(getEmptyPath('res','.png'), img)
+            cv2.rectangle(img, point, (point[0]+template.shape[1], point[1]+template.shape[0]), (0,0,255), 2)
+
+        plt.subplot(1,2,1)
+        plt.title('Convolution')
+        plt.imshow(res, cmap = 'gray')
+        
+        plt.subplot(1,2,2)
+        plt.title('Detection')
+        plt.imshow(img, cmap = 'gray')
+        
+        plt.show()
 
 
     return flag
 
 
+def getEmptyPath(file_name, file_ext, output_path='output'):
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+
+    i = 0
+
+    while os.path.exists(output_path + '/' + file_name + str(i) + file_ext):
+        print
+        i += 1
+    
+    empty_path = output_path + '/' + file_name + str(i) + file_ext
+
+
+    return empty_path
+
+
 
 if __name__ == '__main__':
     
-    result = searchImage(img_path='data/sample/sample_block.png',
+    result = searchImage(img_path='data/sample/sample_block_2.png',
                          template_path='data/labels/organic.png')
 
     print('Result:',result)
-
-
-
-
-
-
-    # template = cv2.imread('mario_coin.png')
-    # w, h = template.shape[:-1]
-
-    # res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
-    # threshold = .8
-    # loc = np.where(res >= threshold)
-    # for pt in zip(*loc[::-1]):  # Switch collumns and rows
-    #     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-
-    # cv2.imwrite('result.png', img_rgb)
