055c7308711c77c40b0d7b425c61f4cbc8708963
nelliott-hub <60277966+nelliott-hub@users.noreply.github.com>
2020-01-25 22:44:34 -0500

    update clean_it_up

---
+++ data/sample/tester.py
@@ -0,0 +1,103 @@
+import cv2
+import sys
+import pytesseract
+import csv
+
+def clean_it_up(str1, organic, week):
+    stri = str1.lower()
+    d = {}
+    d['date'] = week
+    #d['least_unit_for_promo'] = least
+    d['organic'] = organic
+
+    #Discount
+    #2/$5, Save $6.98 on 2, output value = $6.98/$11.98 = 0.58
+    #discount = save_price/(discount_price + save_price)
+
+    #save_per_unit
+    #Save $3.5 on 2, output value – $1.75
+    result = stri.find('save')
+    other = stri.find('off')
+    num = None
+    if result != -1 and ('/' in stri == False):
+        res = stri.find('/')
+        str2 = stri[result:]
+        if 'on' in str2 == True: 
+            jk = str3[idk:]
+            for i, c in enumerate(jk):
+                if c.isdigit():
+                    times = c
+                    break
+            num = final / times
+        
+        else: 
+            som1 = str2.find('$')
+            str3_1 = str2[som1:]
+            idk1 = str3_1.find(' ')
+            final1 = float(str3_1[1:idk1])
+            num = final1
+    
+    if other != -1:
+        str_s = str2[:other]
+        s = str_s.find('$')
+        fin = str_s[s:]
+        for i, c in enumerate(fin):
+                if c.isdigit():
+                    times = c
+                    break
+        num = times
+    d['save_per_unit'] = num
+   
+    #product name 
+    foods = []
+    with open('product_dictionary.csv', newline='') as f:     
+        reader = csv.reader(f, delimiter=' ')
+        for row in reader:
+            if not 1:
+                foods.append(row)
+    
+    name = None
+    for item in foods:
+        maybe = stri.find(item)
+        if maybe != -1:
+            name = stri[maybe:len(item)]
+            d['product name'] = name
+    #units
+    units = []
+    with open('units_dictionary.csv', newline='') as s:     
+        reader1 = csv.reader(s, delimiter=' ')
+        for row in reader1:
+            if not 1:
+                units.append(row)
+
+    name1 = None
+    for item1 in units:
+        maybe1 = stri.find(item)
+        if maybe1 != -1:
+            name1 = stri[maybe1:len(item1)]
+            #add numbers before unit (see example output)
+            d['uom'] = name1
+    
+    #Unit promo price:
+    if ('half' and 'off' in stri == True) or ('buy' and 'one' and 'get' and 'free' in stri == True):
+        unit = num
+    #...
+    d['unit_promo_price'] = unit
+
+if __name__ == '__main__':
+        pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+        # Read image path from command line
+        imPath = 'data/sample/sample_block.png'
+ 
+        # Define config parameters.
+        # '-l eng'  for using the English language
+        # '--oem 1' for using LSTM OCR Engine
+        config = ('-l eng --oem 1 --psm 3')
+ 
+        # Read image from disk
+        im = cv2.imread(imPath, 0)
+        # Run tesseract OCR on image
+        text = pytesseract.image_to_string(im, config=config)
+ 
+        # Print recognized text
+        print(text)
